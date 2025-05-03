b6dda8a5a143d3b468466883238bdefcff6dbb87
Kirill Aleksandrov <kirill.aleks.swe@gmail.com>
2025-05-03 17:04:54 +0200

    Upgrade dns provider

--- dns/domain-provisioning/config.tf
+++
@@ -1,8 +0,0 @@
-provider "dns" {
-  update {
-    server        = var.server
-    key_name      = var.key_name
-    key_algorithm = var.key_algorithm
-    key_secret    = var.key_secret
-  }
-}
--- dns/domain-provisioning/providers.tf
+++ dns/domain-provisioning/providers.tf
@@ -2,7 +2,16 @@ terraform {
   required_providers {
     dns = {
       source  = "hashicorp/dns"
-      version = "3.4.1"
+      version = "3.4.3"
     }
+  }
+}
+
+provider "dns" {
+  update {
+    server        = var.server
+    key_name      = var.key_name
+    key_algorithm = var.key_algorithm
+    key_secret    = var.key_secret
   }
 }
