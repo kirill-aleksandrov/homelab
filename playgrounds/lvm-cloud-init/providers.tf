7bbc07e4e74881d145f17142ca27f4a031b76a71
Kirill Aleksandrov <kirill.aleks.swe@gmail.com>
2025-11-23 23:23:24 +0100

    Upgrade bpg/proxmox provider

--- playgrounds/lvm-cloud-init/providers.tf
+++ playgrounds/lvm-cloud-init/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.60.0"
+      version = "0.87.0"
     }
     dns = {
       source  = "hashicorp/dns"
