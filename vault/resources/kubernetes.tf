b890a592a17c04077bc32ffa16f317ccfc3a2c70
Kirill Aleksandrov <kirill.aleks.swe@gmail.com>
2025-12-07 19:12:26 +0100

    Upgrade provider and remove unused resources

--- vault/resources/kubernetes.tf
+++
@@ -1,4 +0,0 @@
-resource "vault_auth_backend" "kubernetes_cert_manager" {
-  type = "kubernetes"
-  path = "kubernetes-cert-manager"
-}
--- vault/resources/pki.tf
+++ vault/resources/pki.tf
@@ -15,19 +15,3 @@ resource "vault_mount" "homelab_int_ca" {
   default_lease_ttl_seconds = 3600
   max_lease_ttl_seconds     = 31557600
 }
-
-resource "vault_pki_secret_backend_role" "cert_manager_kubernetes_dashboard" {
-  backend                     = vault_mount.homelab_int_ca.path
-  name                        = "cert-manager-kubernetes-dashboard"
-  ttl                         = 3600
-  key_type                    = "ec"
-  key_bits                    = 256
-  allowed_domains             = ["dashboard.kubernetes.homelab"]
-  allow_bare_domains          = true
-  allow_ip_sans               = false
-  allow_wildcard_certificates = false
-  allow_localhost             = false
-  require_cn                  = false
-  key_usage                   = ["DigitalSignature"]
-  ext_key_usage               = ["ServerAuth"]
-}
