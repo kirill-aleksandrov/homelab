68847270bc3c060964def01a266136404ed5cd1d
Kirill Aleksandrov <kirill.aleks.swe@gmail.com>
2026-09-08 22:24:33 +0200

    Update terraform providers and fix ansible deprecation warnings

--- vault/deploy/terraform/providers.tf
+++ vault/deploy/terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.88.0"
+      version = "0.112.0"
     }
     dns = {
       source  = "hashicorp/dns"
-      version = "3.4.3"
+      version = "3.6.1"
     }
   }
 }
